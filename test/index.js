51c56544c5e0ad70b960e40a787d3ec432672a27
FelipeBarrosCruz <felipe.barros.pt@gmail.com>
2016-09-17 07:50:38 -0300

    Fix test task into package.json and add sample test

---
+++ test/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const should = require('chai').should();
+
+describe('Setup Application', () => {
+  
+  it('Should run application', function (done) {
+    let application = null;
+    (application === null).should.equal(true);
+    return done(null);
+  });
+});
